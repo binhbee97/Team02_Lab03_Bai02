f44b3d4eafbc56bc2cae40349b8e0ac2f02a1ccb
Tien Bui <tienbui1272@gmail.com>
2021-09-18 10:29:49 +0700

    file new

---
+++ src/hello/ktrachanle.java
@@ -0,0 +1,19 @@
+package chanle;
+
+public class ktrachanle {
+	
+
+	public class ktrasochanle {
+
+		static boolean isChanLe(int x) {
+			if(x % 2 == 0)
+				return true;
+			else
+				return false;
+		}
+		public static void main(String[] args) {
+			System.out.println(isChanLe(21));
+		}
+	}
+
+}
